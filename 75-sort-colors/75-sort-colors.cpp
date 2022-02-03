203e208d4085bab60941f0ac3fd9faf62c4f5fc0
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-02-03 22:13:40 +0530

    Time: 6 ms (14.49%), Space: 8.4 MB (23.54%) - LeetHub

--- 75-sort-colors/75-sort-colors.cpp
+++ 75-sort-colors/75-sort-colors.cpp
@@ -1,14 +1,45 @@
-
+// ## Bubble Sort
 class Solution {
 public:
-    void sortColors(vector<int>& arr) {
-        int n=arr.size();
-        for(int i=0;i<n-1;i++){
-        for(int j=0;j<n-1-i;j++){
-            if(arr[j]>arr[j+1]){
-                swap(arr[j],arr[j+1]);
+//     void sortColors(vector<int>& arr) {
+//         int n=arr.size();
+//         for(int i=0;i<n-1;i++){
+//         for(int j=0;j<n-1-i;j++){
+//             if(arr[j]>arr[j+1]){
+//                 swap(arr[j],arr[j+1]);
+//             }
+//         }
+//     }
+//     }
+     void sortColors(vector<int>& nums) {
+       int zero = 0;
+        int one = 0;
+        int two = 0;
+        for(int i =0;i<nums.size();i++){
+            if(nums[i] == 0){
+                zero++;
+            }else if(nums[i] == 1){
+                one++;
+            }else{
+                two++;
             }
         }
-    }
+        
+        int t = 0;
+        while(zero--){
+            nums[t] = 0;
+            t++;
+        }
+        while(one--){
+            nums[t] = 1;
+            t++;
+        }
+        while(two--){
+            nums[t] = 2;
+            t++;
+        }
+        
     }
 };
+
+// ## Faster Approach
