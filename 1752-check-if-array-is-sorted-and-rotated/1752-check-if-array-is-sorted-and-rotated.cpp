da2d8732e7d87af9c4d9da07428d9611197753fe
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-02-05 15:18:12 +0530

    Time: 4 ms (47.82%), Space: 8.5 MB (38.04%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.cpp
@@ -1,24 +1,37 @@
 class Solution {
 public:
     bool check(vector<int>& nums) {
-        int i=0;
-        int j=0;
-        while((i<nums.size()-1)  && (j<nums.size()-1)){
-            if(j>i){
-                if(nums[j]>nums[j+1]) return false;
-                j++;
-            }
-            else{
-                if(nums[i]<=nums[i+1]) i++;
-                else{
+//         int i=0;
+//         int j=0;
+//         while((i<nums.size()-1)  && (j<nums.size()-1)){
+//             if(j>i){
+//                 if(nums[j]>nums[j+1]) return false;
+//                 j++;
+//             }
+//             else{
+//                 if(nums[i]<=nums[i+1]) i++;
+//                 else{
                     
-                    j=i+1;
-                     if(nums[0]<nums[nums.size()-1]){
-                         return false;
-                    }
-                }
-            }
+//                     j=i+1;
+//                      if(nums[0]<nums[nums.size()-1]){
+//                          return false;
+//                     }
+//                 }
+//             }
+//         }
+//         return true;
+        
+        
+        // Method2
+        int cnt=0;
+        int n=nums.size();
+        for(int i=0;i<n-1;i++){
+            if(nums[i]>nums[i+1]) cnt++;
         }
-        return true;
+        if(nums[n-1]>nums[0]) cnt++;
+        if(cnt<=1){
+            return true;
+        }
+        return false;
     }
 };
