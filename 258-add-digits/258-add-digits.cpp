6853ec1693f88320f4604e8f81e8daefbc6e14fd
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-02-08 18:35:29 +0530

    Time: 3 ms (53.62%), Space: 5.7 MB (95.57%) - LeetHub

--- 258-add-digits/258-add-digits.cpp
+++ 258-add-digits/258-add-digits.cpp
@@ -1,16 +1,19 @@
 class Solution {
 public:
     int addDigits(int num) {
-        unsigned int sum=num%10;
-        while(num/10>0){  
-            sum=0;
-            while(num!=0){
-                sum=sum+(num%10);
-                num=num/10;
-                cout<<"sum:"<<sum<<" num:"<<num<<endl;
-            }
-            num=sum;
-        }
-        return sum;
+        // unsigned int sum=num%10;
+        // while(num/10>0){  
+        //     sum=0;
+        //     while(num!=0){
+        //         sum=sum+(num%10);
+        //         num=num/10;
+        //         cout<<"sum:"<<sum<<" num:"<<num<<endl;
+        //     }
+        //     num=sum;
+        // }
+        // return sum;
+        if(num<=9) return num;
+        if(num%9==0) return 9;
+        return num%9;
     }
 };
