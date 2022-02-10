3460e7af8dc680c90b2eab4f9e1968ed3a9cb514
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-02-10 18:33:06 +0530

    Time: 144 ms (31.65%), Space: 36.1 MB (87.06%) - LeetHub

---
+++ 560-subarray-sum-equals-k/560-subarray-sum-equals-k.cpp
@@ -0,0 +1,15 @@
+// ## I'll Solve it again When I'll study this topic
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int sum=0, count=0; 
+        unordered_map<int,int> prevsum; 
+        for(auto num:nums){
+            sum+=num;
+            if(sum==k) count++;
+            if(prevsum.find(sum-k)!=prevsum.end()) count+=prevsum[sum-k]; 
+            prevsum[sum]++;
+        }
+        return count;
+    }
+};
