3b1ab765d9c5e457b10435d0bb7eaf0457b38018
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-03-01 13:23:09 +0530

    Time: 8 ms (18.70%), Space: 16.8 MB (6.48%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,23 +1,28 @@
+ void solve(vector<int> nums,vector<int> output, int index, vector<vector<int>> &ans ){
+     if(index>=nums.size()){
+         ans.push_back(output);
+         return;
+     }
+     
+     //Exclude 
+     
+     solve(nums, output, index+1, ans);
+     
+     //Include 
+     int e=nums[index];
+     output.push_back(e);
+     solve(nums, output, index+1, ans);
+     
+ }
+
 class Solution {
 public:
-    void findsub( vector<vector<int>> &res , vector<int> nums , int i){
-        int j = 0;
-        vector<int>v;
-        while(i>0){
-            if((i&1)==1) v.push_back(nums[j]);
-            j++;
-            i = i>>1;
-        }
-        res.push_back(v);
-        return;
-    }
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> res;
-        int n = nums.size();
-        for(int i = 0 ; i < (1<<n) ;i++){
-            findsub(res,nums,i);
-        }
-        return res;
+        vector<vector<int>> ans;
+        vector<int> output;
+        int index=0;
+        
+        solve(nums, output,index, ans);
+        return ans;
     }
-    
 };
