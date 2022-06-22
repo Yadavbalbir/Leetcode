2f4c97bffc4250142c76cc4802ba8b33efcc853b
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-06-22 15:09:32 +0530

    Time: 20 ms (55.85%), Space: 5.9 MB (31.84%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,22 +1,14 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if(x<0) return false;
-        vector<int> num;
-        while(x!=0){
-            num.push_back(x%10);
-            x/=10;
-        }
-        vector<int> num1(num);
-      
-        int s=0;
-        int e=num.size()-1;
-        while(s<e){
-            swap(num[s++],num[e--]);
-        }
-        
-        for(int i=0;i<num.size();i++){
-            if(num[i]!=num1[i]) return false;
+        string s = to_string(x);
+        int i=0, j= s.size()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
         }
         return true;
     }
