3310bd111a9e7fc6568bccd7588435bada89553e
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-06-23 16:43:28 +0530

    Time: 8 ms (22.62%), Space: 7.2 MB (69.73%) - LeetHub

--- 78-subsets/78-subsets.cpp
+++ 78-subsets/78-subsets.cpp
@@ -1,28 +1,29 @@
- void solve(vector<int> nums,vector<int> output, int index, vector<vector<int>> &ans ){
-     if(index>=nums.size()){
-         ans.push_back(output);
-         return;
-     }
-     
-     //Exclude 
-     
-     solve(nums, output, index+1, ans);
-     
-     //Include 
-     int e=nums[index];
-     output.push_back(e);
-     solve(nums, output, index+1, ans);
-     
- }
-
 class Solution {
+vector<int> sol;
+vector<vector<int>> ans;
+
+
+void rec(vector<int> &arr, int level, int n){
+	if(level==n){
+		//base case
+		ans.push_back(sol);
+		return;
+	}
+	
+		// take
+		sol.push_back(arr[level]);
+		rec(arr, level+1, n);
+		sol.pop_back();
+
+		// don't take
+		rec(arr, level+1, n);
+
+}
+    
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>> ans;
-        vector<int> output;
-        int index=0;
+        rec(nums, 0, nums.size());
         
-        solve(nums, output,index, ans);
         return ans;
     }
 };
