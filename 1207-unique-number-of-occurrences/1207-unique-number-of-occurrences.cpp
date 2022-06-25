783ff70074edf5c90904865abd0c7714da2ee7c0
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-06-25 12:59:31 +0530

    Time: 4 ms (61.84%), Space: 8.3 MB (6.47%) - LeetHub

--- 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
+++ 1207-unique-number-of-occurrences/1207-unique-number-of-occurrences.cpp
@@ -2,38 +2,20 @@ class Solution {
 public:
     bool uniqueOccurrences(vector<int>& arr) {
         
-        int n=arr.size();
-        int counter[n];
-        fill_n(counter,n,0);
-        int seen[n];
-        fill_n(seen,n,0);
-        int idx=0; 
-        for(int i=0;i<n;i++){
-            int newfound=0;
-            for(int j=0;j<n;j++){
-                if(arr[i]==arr[j] && seen[j]==0){
-                    counter[idx]++; 
-                    seen[j]=1;
-                    newfound=1;
-                }
-            }
-            if(newfound==1){
-                idx++;
-            }
-        }
-        for(int i=0;i<idx;i++){
-            cout<< counter[i]<<" ";
+        map<int,int> mp;
+        
+        for(auto v: arr){
+            mp[v]++;
         }
         
-     
-        for(int i=0;i<idx;i++){
-            for(int j=i+1;j<idx;j++){
-                if(counter[i]==counter[j]){
-                    return false;
-                }
-            }
+        int cnt =0;
+        set<int> ans;
+        for(auto v: mp){
+            cnt++;
+            ans.insert(v.second);
         }
-        return true;
         
+        if(cnt==ans.size()) return 1;
+        else return 0;
     }
 };
