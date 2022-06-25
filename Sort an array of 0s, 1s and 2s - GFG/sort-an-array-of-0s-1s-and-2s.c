02db6d886112b528b2ac7ca6aa3182142d056cae
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-06-25 13:19:11 +0530

    Added solution - LeetHub

---
+++ Sort an array of 0s, 1s and 2s - GFG/sort-an-array-of-0s-1s-and-2s.c
@@ -0,0 +1,54 @@
+// { Driver Code Starts
+//Initial Template for C
+
+#include <stdio.h>
+
+
+ // } Driver Code Ends
+//User function Template for C
+
+void sort012(int a[], int n)
+{
+    int zeroes =0, ones = 0, twos=0;
+    for(int i=0;i<n;i++){
+        if(a[i]==0) zeroes++;
+        else if(a[i]==1) ones++;
+        else twos++;
+    }
+    
+    int i=0;
+    for(i; i<zeroes;i++){
+        a[i]=0;
+    }
+    for(i;i<zeroes+ones;i++){
+        a[i]=1;
+    }
+    for(i;i<n;i++){
+        a[i]=2;
+    }
+}
+
+// { Driver Code Starts.
+
+int main() {
+
+    int t;
+    scanf("%d", &t);
+
+    while(t--){
+        int n;
+        scanf("%d", &n);
+        int arr[n];
+        for(int i=0;i<n;i++){
+            scanf("%d", &arr[i]);
+        }
+
+        sort012(arr, n);
+
+        for (int i = 0; i < n; i++)
+            printf("%d ", arr[i]);
+        printf("\n");
+    }
+    return 0;
+}
+  // } Driver Code Ends
