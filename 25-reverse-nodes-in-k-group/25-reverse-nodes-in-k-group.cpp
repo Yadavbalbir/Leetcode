5ad5d74621e11704b4fdeb4460bc581b37220360
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-06-26 20:22:20 +0530

    Time: 43 ms (5.15%), Space: 11.6 MB (30.50%) - LeetHub

--- 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
+++ 25-reverse-nodes-in-k-group/25-reverse-nodes-in-k-group.cpp
@@ -9,27 +9,44 @@
  * };
  */
 class Solution {
+    
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-        int cnt = 1;
-        ListNode *temp = head, *leftNode = head , *ansNode = head;
-        while(temp->next){
+        
+        if(head==NULL){
+            return NULL;
+        }
+        
+        ListNode* prev = NULL;
+        ListNode *currNode = head;
+        ListNode *next = NULL;
+        
+        int cnt =0;
+        ListNode* temp = head;
+        int count =0;
+        
+        
+        while(temp!=NULL){
+            count++;
             temp = temp->next;
+        }
+        if(count<k){
+            return head;
+        }
+        
+        while(cnt<k && currNode!=NULL){
+            next = currNode->next;
+            currNode->next = prev;
+            prev = currNode;
+            currNode = next;
             cnt++;
+        }    
+        
+
+        
+        if(next!=NULL){
+            head->next = reverseKGroup(next, k);
         }
-        cnt = cnt/k;
-        while(cnt--){
-            vector<int> v;
-            for(int i=0; i<k; i++){
-                v.push_back(leftNode->val);
-                leftNode = leftNode->next;
-            }
-            for(int i=0;i<k;i++){
-                ansNode->val = v.back();
-                v.pop_back();
-                ansNode = ansNode->next;
-            }
-        }
-        return head;
+        return prev;
     }
 };
