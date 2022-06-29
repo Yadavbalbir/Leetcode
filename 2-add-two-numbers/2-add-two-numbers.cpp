b7b7ec9f6520d81b94db9fe93e90919e5fc89706
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-06-30 03:18:13 +0530

    Time: 61 ms (36.65%), Space: 71.3 MB (80.97%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.cpp
+++ 2-add-two-numbers/2-add-two-numbers.cpp
@@ -12,103 +12,70 @@ class Solution {
 public:
    
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        vector<int> num1;
-        vector<int> num2;
+        
+        int carry = 0;
+        int sum = l1->val + l2->val;
+        if(sum>9){
+            carry = 1;
+            sum = sum%10;
+        }
+        
+        ListNode* ans = new ListNode(sum);
+        ListNode* head = ans;
+        l1 = l1->next;
+        l2 = l2->next;
+        
+        while(l1!=NULL && l2!=NULL){
+             sum = l1->val + l2->val + carry;
+             l1 = l1->next;
+             l2 = l2->next;
+             if(sum>9){
+                carry = 1;
+                sum = sum%10;
+            }else{
+                 carry=0;
+             }
+            ListNode* nNode = new ListNode(sum);
+            head->next = nNode;
+            head = nNode;
+        }
         
         while(l1!=NULL){
-            num1.push_back(l1->val);
+             sum = l1->val + carry;
             l1 = l1->next;
+             if(sum>9){
+                carry = 1;
+                sum = sum%10;
+            }else{
+                 carry=0;
+             }
+            ListNode* nNode = new ListNode(sum);
+            head->next = nNode;
+            head = nNode;
+            
         }
         
         while(l2!=NULL){
-            num2.push_back(l2->val);
-            l2 = l2->next;
-        }
-        for(auto v: num1){
-            cout<<v<<" ";
-        }
-        cout<<endl;
-        
-        for(auto v: num2){
-            cout<<v<<" ";
-        }
-        cout<<endl;
-        
-        vector<int> ans;
-        int carry = 0;
-        if(num1.size()>=num2.size()){
-            int n1 = num1.size();
-            int n2 = num2.size();
-            for(int i=0;i< n2;i++){
-                int sum = num1[i] + num2[i]+carry;
-                cout<<sum<<endl;
-                if(sum>9){
-                    carry = 1;
-                    sum = sum%10;
-                }else{
-                    carry = 0;
-                }
-                ans.push_back(sum);
-            }
-            for(int i = n2; i< n1; i++){
-                int sum = num1[i]+carry;
-                if(sum>9){
-                    carry = 1;
-                    sum = sum%10;
-                }else{
-                    carry = 0;
-                }
-                ans.push_back(sum);
-                
-            }
-        }else{
-            int n1 = num1.size();
-            int n2 = num2.size();
-            for(int i=0;i<n1;i++){
-                int sum = num1[i] + num2[i]+carry;
-              
-                if(sum>9){
-                    carry = 1;
-                    sum = sum%10;
-                }else{
-                    carry = 0;
-                }
-                ans.push_back(sum);
-            }
-            for(int i = n1; i<n2; i++){
-                int sum = num2[i]+carry;
-                if(sum>9){
-                    carry = 1;
-                    sum = sum%10;
-                }else{
-                    carry = 0;
-                }
-                ans.push_back(sum);
-                
-            }
+             sum = l2->val + carry;
+             l2 = l2->next;
+             if(sum>9){
+                carry = 1;
+                sum = sum%10;
+            }else{
+                 carry=0;
+             }
+            ListNode* nNode = new ListNode(sum);
+            head->next = nNode;
+            head = nNode;
         }
         
-        if(carry!=0) ans.push_back(carry);
-        
-        for(auto v: ans){
-            cout<<v<<" ";
+        if(carry!=0){
+            ListNode* nNode = new ListNode(carry);
+            head->next = nNode;
+            head = nNode;
         }
         
-        reverse(ans.begin(), ans.end());
-        
-        ListNode* result = new ListNode(ans.back());
-        ans.pop_back();
-        
-        ListNode* head = result;
-        
-        while(ans.size()!=0){
-            ListNode* ntemp= new ListNode(ans.back());
-            ans.pop_back();
-            head->next = ntemp;
-            head = ntemp;
-        }
-        
-        return result;
+        return ans;
         
     }
 };
