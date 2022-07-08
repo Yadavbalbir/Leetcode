7735e047a336b9f01e1314cf2aebcbb50a74fc32
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-07-09 03:20:48 +0530

    Time: 2 ms (44.51%), Space: 5.9 MB (64.56%) - LeetHub

--- 367-valid-perfect-square/367-valid-perfect-square.cpp
+++ 367-valid-perfect-square/367-valid-perfect-square.cpp
@@ -1,12 +1,25 @@
 class Solution {
 public:
     bool isPerfectSquare(int num) {
-        for(long long int i=0;(i*i)<=num;i++){
-           
-            if((i*i)==num){
+        
+        long long start = 1;
+        long long end = num;
+        long long mid = start + (end-start)/2;
+        
+        
+        while(start<=end){
+            if(mid*mid==num){
                 return true;
+            }else if(mid*mid<num){
+                start = mid+1;
+            }else{
+                end = mid-1;
             }
+            
+            mid = start + (end-start)/2;
         }
+        
         return false;
+        
     }
 };
