7a2e10c0cf5b64b99a4aa54dbc3aba3ea7f3f3c5
Balbir Yadav <balbir.iitbombay3602@gmail.com>
2022-07-30 17:18:44 +0530

    Time: 15 ms (31.77%), Space: 10.3 MB (8.69%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,7 +1,20 @@
 class Solution {
 public:
-    int fib(int n) {
+    int dp[1000100];
+    int F(int n){
         if(n<=1) return n;
-        else return fib(n-1)+fib(n-2);
+        
+        // if have it solved
+        if(dp[n]!=-1){
+            return dp[n];
+        }
+        
+        //doing computation
+       return dp[n]= F(n-1)+F(n-2);
+    }
+    
+    int fib(int n) {
+        memset(dp,-1,sizeof(dp));
+        return F(n);
     }
 };
